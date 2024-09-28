b25ee3d5b28c290df92e85d6c2aad82bcc52ceb8
nazrinara6 <nazrinara6@gmail.com>
2024-09-28 19:57:13 +0600

    Assignment

---
+++ Assignment/Assignment_24_char.java
@@ -0,0 +1,20 @@
+package core_java_assignment;
+
+public class Assignment_24_char {
+
+	public static void main(String[] args) {
+		char grades[] = new char[5];
+		grades[0]= 'A';
+		grades[1]= 'B';
+		grades[2]= 'C';
+		grades[3]= 'D';
+		grades[4]= 'E';
+		
+		for(int i=0;i<5;i++)
+		{
+			System.out.println(grades[i]);
+		}
+
+	}
+
+}
